d3d06bbaa7c245a9912191ba563aaa62e908e6c5
AIRIA <chaibingcheng0305@163.com>
2013-09-01 14:41:16 +0800

    添加了了逻辑方法 1.检测钻石是不是可以被消除 2.检测生成的钻石列表是否有效

--- Classes/scenes/PlayScene.cpp
+++ Classes/scenes/PlayScene.cpp
@@ -2,10 +2,7 @@
 
 using namespace std;
 
-#define ROW 8
-#define COL 8
-#define DIAMOND_WIDTH 60
-#define DIAMOND_HEIGHT 60
+
 
 bool PlayScene::init()
 {
@@ -13,14 +10,14 @@ bool PlayScene::init()
     do
     {
         CC_BREAK_IF(!BaseScene::init());
-		//加载纹理
+        //加载纹理
         frameCache->addSpriteFramesWithFile("textures/diamond.plist");
         CCSprite *bgSpr = SPRITE(hdpi_bground1.jpg);
         bgSpr->setPosition(VisibleRect::center());
         addChild(bgSpr);
-		res = true;
-		createDiamonds();
-		checkInitDiamonds();
+        res = true;
+        createDiamonds();
+        checkInitDiamonds();
     }
     while (0);
     return res;
@@ -28,67 +25,141 @@ bool PlayScene::init()
 
 void PlayScene::draw()
 {
-	//在这里绘制网格
+    //在这里绘制网格
 }
 
 void PlayScene::checkInitDiamonds()
 {
-	CCObject *oneRow = NULL;
-	CCARRAY_FOREACH(diamonds,oneRow)
-	{
-		CCArray *ds = (CCArray*)oneRow;
-		CCObject *diamond;
-		//相同钻石挨着的个数
-		int num=1;
-		int prevType=0;
-		CCARRAY_FOREACH(ds,diamond)
-		{
-			DiamondSprite *diamondSpr = (DiamondSprite*)diamond;
-			//CCLog("row %d",diamondSpr->row);
-			if(prevType==0){
-				prevType = diamondSpr->type;
-			}else if(prevType==diamondSpr->type){
-				num++;
-			}else{
-				num = 1;
-				prevType = diamondSpr->type;
-			}
-			if(num==3){
-				CCLog("find 3diamond  at row %d",diamondSpr->row);
-				srand(time(NULL));
-				int newType = prevType;
-				while(newType==prevType){
-					newType = rand()%(COL-1)+1;
-				}
-				prevType=newType;
-				num = 1;
-			}
-		}
-	}
+	checkThree(diamonds);
+	setRotateDiamonds();
+	CCLog("---------");
+	checkThree(rotateDiamonds);
 }
 
 void PlayScene::createDiamonds()
 {
-	srand(time(0));
-	CCSpriteBatchNode *diamondBatch = CCSpriteBatchNode::createWithTexture(SPRITE_FRAME(hdpi_jewel1.png)->getTexture());
-	addChild(diamondBatch);
-	diamonds = CCArray::createWithCapacity(ROW);
-	for(int i=0;i<ROW;i++){
-		CCArray *col = CCArray::createWithCapacity(COL);
-		for(int j=0;j<COL;j++){
-			int feed = rand()%(COL-1)+1;
-			CCString *frameName = CCString::createWithFormat("hdpi_jewel%d.png",feed);
-			DiamondSprite *diamond = DiamondSprite::createDiamond(frameName->getCString());
-			diamond->type = feed;
-			diamond->row = i;
-			diamond->col = j;
-			col->addObject(diamond);
-			cout << feed << " ";
-			diamond->setPosition(ccp(DIAMOND_WIDTH*(j+0.5),VisibleRect::leftTop().y-DIAMOND_HEIGHT*(i+0.5)));
-			diamondBatch->addChild(diamond);
+    srand(time(0));
+    CCSpriteBatchNode *diamondBatch = CCSpriteBatchNode::createWithTexture(SPRITE_FRAME(hdpi_jewel1.png)->getTexture());
+    addChild(diamondBatch);
+    for(int i=0; i<D_ROW; i++)
+    {
+        for(int j=0; j<D_COL; j++)
+        {
+            int feed = rand()%(D_TYPE-1)+1;
+            CCString *frameName = CCString::createWithFormat("hdpi_jewel%d.png",feed);
+            DiamondSprite *diamond = DiamondSprite::createDiamond(frameName->getCString());
+            diamond->type = feed;
+            diamond->row = i;
+            diamond->col = j;
+            diamonds[i][j] = diamond;
+            diamond->setPosition(ccp(DIAMOND_WIDTH*(j+0.5),VisibleRect::leftTop().y-DIAMOND_HEIGHT*(i+0.5)));
+            diamondBatch->addChild(diamond);
+        }
+    }
+}
+
+void PlayScene::setRotateDiamonds()
+{
+    for(int i=0; i<D_ROW; i++)
+    {
+        for(int j=0; j<D_COL; j++)
+        {
+            rotateDiamonds[i][j] = diamonds[D_ROW-j-1][D_COL-i-1];
+        }
+    }
+}
+
+void PlayScene::checkThree( DiamondSprite *source[D_ROW][D_COL] )
+{
+	srand(time(NULL));
+	DiamondSprite **ds;
+	ds = source[0];
+	for(int i=0; i<D_ROW; i++)
+	{
+		int prevType = 0;
+		int num = 1;
+		for(int j=0; j<D_COL; j++)
+		{
+			if(checkIsVailible(*ds))
+			{
+				CCLog("is availible at row %d,col%d",(*ds)->row,(*ds)->col);
+			}
+			int currentType = (*ds)->type;
+			//CCLog("diamond type %d",currentType);
+			if(prevType==0)
+			{
+				prevType = currentType;
+			}
+			else if (prevType==currentType)
+			{
+				num++;
+			}
+			else
+			{
+				num = 1;
+				prevType = currentType;
+			}
+			if(num==3){
+				CCLog("find 3 diamonds at row %d",(*ds)->row);
+				while(prevType==currentType){
+					currentType = rand()%(D_TYPE-1)+1;
+				}
+			}
+			ds++;
 		}
-		cout << endl;
-		diamonds->addObject(col);
 	}
 }
+
+bool PlayScene::checkIsVailible( DiamondSprite *ds )
+{
+	int row = ds->row,col=ds->col,type = ds->type;
+	int t1,t2;
+	if(row>0&&row<(D_ROW-1)){//y direction
+		t1 = diamonds[row-1][col]->type;
+		t2 = diamonds[row+1][col]->type;
+		if(t1==t2&&t1==type){
+			return true;
+		}
+	}
+	if(col>0&&col<(D_COL-1)){//x direction
+		t1 = diamonds[row][col-1]->type;
+		t2 = diamonds[row][col+1]->type;
+		if(t1==t2&&t1==type){
+			return true;
+		}
+	}
+	if(row>1){//up
+		t1 = diamonds[row-1][col]->type;
+		t2 = diamonds[row-2][col]->type;
+		if(t1==t2&&t1==type){
+			return true;
+		}
+	}
+	if(row<(D_ROW-2)){//down
+		t1 = diamonds[row+1][col]->type;
+		t2 = diamonds[row+2][col]->type;
+		if(t1==t2&&t1==type){
+			return true;
+		}
+	}
+	
+	if(col>1){//left
+		t1 = diamonds[row][col-1]->type;
+		t2 = diamonds[row][col-2]->type;
+		if(t1==t2&&t1==type){
+			return true;
+		}
+	}
+
+	if(col<(D_COL-2)){//right
+		t1 = diamonds[row][col+1]->type;
+		t2 = diamonds[row][col+2]->type;
+		if(t1==t2&&t1==type){
+			return true;
+		}
+	}
+	return false;
+}
+
+
 
--- Classes/scenes/PlayScene.h
+++ Classes/scenes/PlayScene.h
@@ -4,18 +4,29 @@
 #include "base/BaseScene.h"
 #include "components/DiamondSprite.h"
 
+#define D_ROW 8
+#define D_COL 8
+#define DIAMOND_WIDTH 60
+#define DIAMOND_HEIGHT 60
+#define D_TYPE 8
 
 class PlayScene:public BaseScene
 {
 public:
-	CCArray *diamonds;
+	DiamondSprite *diamonds[D_ROW][D_COL];
+	DiamondSprite *rotateDiamonds[D_ROW][D_COL];
 public:
+	/* 检测一个宝石是不是可以被删除 */
+	bool checkIsVailible(DiamondSprite *ds);
 	/* 检测生成的钻石列表中是不是有可以消除的钻石 */
 	void checkInitDiamonds();
 	/* 创建随机顺序的8*8的钻石 */
 	void createDiamonds();
+	/* 旋转数组 */
+	void setRotateDiamonds();
+
+	void checkThree(DiamondSprite *source[D_ROW][D_COL]);
 public:
-	PlayScene():diamonds(NULL){};
     virtual bool init();
 	virtual void draw();
     CREATE_FUNC(PlayScene);
